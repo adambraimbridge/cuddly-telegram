35bc7f8ba1aa62851112fb15210387af882ae1d6
Adam Braimbridge <adam.braimbridge@ft.com>
2019-05-31 17:24:31 +0100

    Friday hometime

---
+++ model.js
@@ -0,0 +1,31 @@
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/About
+export const decks = [
+  {
+    name: 'Standard built-in objects',
+    description: ''
+  },
+  {
+    name: 'Statements and declarations',
+    description: ''
+  },
+  {
+    name: 'Expressions and operators',
+    description: ''
+  },
+  {
+    name: 'Functions',
+    description: ''
+  },
+  {
+    name: 'Classes',
+    description: ''
+  },
+  {
+    name: 'Errors',
+    description: ''
+  },
+  {
+    name: 'New in JavaScript',
+    description: ''
+  }
+];
